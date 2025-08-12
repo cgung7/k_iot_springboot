c7a2d30c4a8ca844fffb6ea180110a6c3ac8a8ac
cgung7 <cgung7@naver.com>
2025-08-12 14:52:29 +0900

    20250812SB

---
+++ src/main/java/com/example/k5_iot_springboot/repository/B_StudentRepository.java
@@ -0,0 +1,4 @@
+package com.example.k5_iot_springboot.repository;
+
+public interface B_StudentRepository {
+}
---
+++ src/main/java/com/example/k5_iot_springboot/service/impl/B_StudentServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.k5_iot_springboot.service.impl;
+
+public class B_StudentServiceImpl {
+}
