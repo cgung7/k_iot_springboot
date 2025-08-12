34b91220929321c3c9946e47a1a2c766be58fb96
cgung7 <cgung7@naver.com>
2025-08-12 16:27:25 +0900

    20250812SB

--- src/main/java/com/example/k5_iot_springboot/dto/B_Student/StudentResponseDto.java
+++ src/main/java/com/example/k5_iot_springboot/dto/B_Student/StudentResponseDto.java
@@ -1,4 +1,14 @@
 package com.example.k5_iot_springboot.dto.B_Student;
 
-public class StudentResponse {
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter // 필수 - JSON 변환 시 필드 접근을 위해 사용
+@Builder
+@AllArgsConstructor
+public class StudentResponseDto {
+    private Long id;
+    private String name;
 }
